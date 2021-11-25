7c66c5be222a5d6eb465e66a8a179f3d864de956
Ray Plante <raymond.plante@nist.gov>
2021-11-24 23:47:15 -0500

    setup.py: increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os, sys, subprocess, unittest
 from setuptools import setup
 
 setup(name='multibag',
-      version='0.1',
+      version='0.2',
       description="multibag: a Python-based reference implementation of the Multibag BagIt profile",
       author="Ray Plante",
       author_email="raymond.plante@nist.gov",
